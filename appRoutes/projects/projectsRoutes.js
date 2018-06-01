ff8c96cc19c01347afacb42ae6d453f68f5fdb28
Iqra Javed <iqrajaved@Iqras-MacBook-Air.local>
2018-06-01 16:12:33 -0500

    implemented POST for projects

--- appRoutes/projects/projectsRoutes.js
+++ appRoutes/projects/projectsRoutes.js
@@ -27,5 +27,25 @@ router.get('/:id', (req, res) => {
         })
 })
 
+// POST REQUEST
+router.post('/', (req, res) => {
+    const {name, description, completed} = req.body;
+    if(!name || name.length > 128) {
+        res.status(400).json({error: 'Please provide a name for the project'})
+        return;
+    }
+    if(!description || description.length > 128) {
+        res.status(400).json({error: 'Please provide a description for the project'})
+        return;
+    }
+
+    projectModel.insert({name, description, completed})
+        .then(response => {
+            res.status(201).json(response)
+        })
+        .catch(error => {
+            res.status(500).json({error: 'There was an error saving the new project.'})
+        })
+})
 
 module.exports = router;
